0fa6d559f170fb0ed6d0754651496e159d57cc0c
Ruben Jimenez Andreu <rubenjmnz93@gmail.com>
2023-11-12 19:31:15 +0100

    Added recipe model and recipe API.

---
+++ app/recipe/urls.py
@@ -0,0 +1,16 @@
+"""
+URL mapping for the recipe APIs.
+"""
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
